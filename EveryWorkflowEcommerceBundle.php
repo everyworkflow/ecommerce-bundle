27ab8cf9880a798fcea3fe0725f61b7b5b44241c
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:03:45 +0000

    Added ?ExtensionInterface

--- EveryWorkflowEcommerceBundle.php
+++ EveryWorkflowEcommerceBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\EcommerceBundle;
 
 use EveryWorkflow\EcommerceBundle\DependencyInjection\EcommerceExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowEcommerceBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new EcommerceExtension();
     }
